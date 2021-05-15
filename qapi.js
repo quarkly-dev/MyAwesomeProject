3bfd0c46381e4b7a9018f456eda80c29f3a6c8a0
quarkly-dev <dev@quarkly.io>
2021-05-15 14:51:02 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"609fb53b7c7803001e32f36a",
+				"609fb53b7c7803001e32f36c"
+			]
+		},
+		"609fb53b7c7803001e32f36a": {
+			"id": "609fb53b7c7803001e32f36a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"609fb53b7c7803001e32f36c": {
+			"id": "609fb53b7c7803001e32f36c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
